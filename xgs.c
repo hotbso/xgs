85c68fea1e69aea0f0fd7ca881be95373d0514f2
Holger Teutsch <holger.a.teutsch@gmail.com>
2021-12-06 11:59:50 +0100

    fix regression: the A319 does not use GearStrutCompressDist_m

--- xgs.c
+++ xgs.c
@@ -20,7 +20,7 @@
 #include <acfutils/assert.h>
 #include <acfutils/airportdb.h>
 
-#define VERSION "3.41"
+#define VERSION "3.42"
 
 static float flight_loop_cb(float inElapsedSinceLastCall,
                 float inElapsedTimeSinceLastFlightLoop, int inCounter,
@@ -177,12 +177,15 @@ static void get_acf_dr()
     if (toliss_vls_dr) {
         logMsg("ToLiss A3xx detected");
         acf_ias_dr = XPLMFindDataRef("AirbusFBW/IASCapt");
-        toliss_strut_compress_dr = XPLMFindDataRef("AirbusFBW/GearStrutCompressDist_m");
-        toliss_a340 = (0 == strncmp(acf_icao, "A34", 3));
-        if (toliss_strut_compress_dr == NULL)
-            logMsg(".. but toliss_strut_compress_dr is not defined");
-    } else {
-        acf_ias_dr = toliss_strut_compress_dr = NULL;
+
+        /* the A319 does not use the GearStrutCompressDist_m dr */
+        toliss_strut_compress_dr = NULL;
+        if (0 != strncmp(acf_icao, "A319", 4)) {
+            toliss_strut_compress_dr = XPLMFindDataRef("AirbusFBW/GearStrutCompressDist_m");
+            toliss_a340 = (0 == strncmp(acf_icao, "A34", 3));
+            if (toliss_strut_compress_dr == NULL)
+                logMsg(".. but toliss_strut_compress_dr is not defined");
+        }
     }
 }
 
